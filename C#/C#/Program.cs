4cd3c60a24f640fe70e3f6617eaa6d4d1aaa0eae
tomlazen <oismarilo@gmail.com>
2023-03-06 22:00:01 +0100

    added loops, classes, methods

--- C#/C#/Program.cs
+++ C#/C#/Program.cs
@@ -155,6 +155,27 @@ namespace C_
             names[2] = "Dylan";
             Console.WriteLine(names[0] + "\n");
 
+            // Crate an array where you specify only dimensions, and will insert values later.
+            int[] someRandomArray = new int[3];
+            someRandomArray[0] = 10;
+
+            // 2D ARRAYS
+            // Use comma to indicate 2D array.
+            int[,] twodArray = {
+                { 10, 20, 30, 40},
+                { 100, 200, 300, 400}
+            };
+
+            // container.Length - gets length across all dimensions
+            // container.Getlength(dimension) - gets length of specified dimension
+            for (int i = 0; i < twodArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < twodArray.GetLength(1); j++)
+                {
+                    Console.WriteLine(twodArray[i, j]);
+                }
+            }
+
 
             // FUNCTIONS (calling them)
             SayHi("Pepo", 40);
@@ -165,12 +186,13 @@ namespace C_
             Console.WriteLine(Cube(number: 4));
 
             int number_assignment = Cube(number: 3); // possible as a variable
-            Console.WriteLine(number_assignment + "\n");
+            Console.WriteLine(number_assignment);
 
+            Console.WriteLine($"Pow function: {Power(4, 3)}\n");
 
             // IF ELSE
             bool isMale = true;
-            bool isTall = false; 
+            bool isTall = false;
 
             // && means AND
             if (isMale && isTall)
@@ -194,17 +216,150 @@ namespace C_
             // || means OR operator
             if (isMale || isTall)
             {
-                Console.WriteLine("You are either a man or you are tall.");    
+                Console.WriteLine("You are either a man or you are tall.");
             }
 
             int some_var = 2;
             if (some_var > 0)
             {
-                Console.WriteLine("The number is positive\n");
+                Console.WriteLine("The number is positive.\n");
             }
 
 
-            // ------------
+            // SWITCH
+            string textos = "1";
+            switch (textos)
+            {
+                case "1":
+                    Console.WriteLine("The best\n");
+                    break;
+                case "2":
+                    Console.WriteLine("Medium\n");
+                    break;
+                case "3":
+                    Console.WriteLine("The worst\n");
+                    break;
+                // Default is used when no case is matched.
+                default:
+                    Console.WriteLine("You specified wrong number.\n");
+                    break;
+            }
+
+
+            // WHILE LOOP, DO WHILE LOOP, FOR LOOP
+            // WHILE LOOP - checks the condition and then continues with code or it breaks
+            int index1 = 0;
+            while (index1 <= 4)
+            {
+                Console.WriteLine($"The index is {index1}");
+                index1++;
+            }
+            Console.WriteLine("\n");
+
+            // DO WHILE LOOP - executes the code and then checks the condition
+            int index2 = 0;
+            do
+            {
+                Console.WriteLine($"The index is {index2}");
+                index2++;
+            }
+            while (index2 <= 4);
+            Console.WriteLine("\n");
+
+            // FOR LOOP
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Iteration number {i}");
+            }
+            Console.WriteLine("\n");
+
+            // seems like the "i" variable is local to each for loop
+            int[] some_numbers = { 10, 20, 30, 40 };
+            for (int i = 0; i < some_numbers.Length; i++)
+            {
+                Console.WriteLine($"Item: {some_numbers[i]}");
+            }
+            Console.WriteLine("\n");
+
+
+            // COMMENTS
+            // One line comment is just with //
+
+            /*
+             * This is 
+             * a multiline
+             * comment
+             */
+
+            /*
+            It is also 
+            possible to
+            write without
+            asterisk at each line
+            */
+
+
+            // EXCEPTIONS
+            int number1 = 3;
+            int number2 = 0;
+
+            try
+            {
+                Console.WriteLine(number1 / number2);
+            }
+            catch (Exception msg) // Catch Exception message as a variable "msg".
+            {
+                Console.WriteLine(msg.Message);
+            }
+
+            // Specify exact error,
+            //  specify multiple exceptions,
+            //  specify finally block, which executes always
+            try
+            {
+                Console.WriteLine(number1 / number2);
+            }
+            catch (DivideByZeroException msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+            catch (IndexOutOfRangeException msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+            finally { Console.WriteLine("Executing 'finally' block."); }
+
+
+            // CLASSES
+
+            // Without constructor, you can asign new values like this below,
+            // but it is not good practise:
+            Book book1 = new Book();
+            book1.title = "Jak Jarda ztratil pivsona.";
+            book1.author = "Jarda";
+            book1.numberOfPages = 300;
+            Console.WriteLine(book1.title);
+
+            // With constructor, good practise.
+            Magazine magazine1 = new Magazine(aTitle: "Playboy",
+                                              aAuthor: "Larry",
+                                              aNumberOfPages: 30);
+            Console.WriteLine(magazine1.title);
+
+            // Here it is possible to create a new instance of a class
+            //  because Magazine class has also second constructor without
+            //  defined paramters.
+            Magazine magazine2 = new Magazine();
+
+            // OBJECT METHODS
+            Student student1 = new Student(aName: "Tom", aAge: 36, aId: 4590);
+            Student student2 = new Student(aName: "Bob", aAge: 34, aId: 4591);
+
+            Console.WriteLine($"Is older: {student1.IsAboveThirtyFive()}");
+            Console.WriteLine($"Is older: {student2.IsAboveThirtyFive()}");
+            Console.WriteLine();
+
+            // ------------------------------------------------------------------------
             // Without this line console window would just immediatelly close,
             //  after we executed the code.
             // It actually waits for user input, but when you just hit enter. The
@@ -228,6 +383,17 @@ namespace C_
         static int Cube(int number)
         {
             int result = number * number * number;
+            return result;
+        }
+
+        // Function to raise base number to the power of exponent.
+        static int Power(int base_number, int exponent)
+        {
+            int result = base_number;
+            for (int i = 1; i < exponent; i++)
+            {
+                result *= base_number;
+            }
             return result;
         }
 
