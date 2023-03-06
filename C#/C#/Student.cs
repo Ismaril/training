4c8b2bf092af9a7d2698209f514f16793d000edc
tomlazen <oismarilo@gmail.com>
2023-03-06 21:59:01 +0100

    first commit

---
+++ C#/C#/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class Student
+    {
+        // class attributes
+        public string name;
+        public int age;
+        public int id;
+
+        // constructor
+        public Student(string aName, int aAge, int aId) { 
+            name = aName;
+            age = aAge;
+            id = aId;
+        }
+
+        // method
+        public bool IsAboveThirtyFive()
+        {
+            return age > 35;
+        }
+    }
+}
