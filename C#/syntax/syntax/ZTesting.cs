a1ef756a98f0291fe929c3e24aae636c2ba1d8cb
tomlazen <oismarilo@gmail.com>
2023-03-16 21:57:15 +0100

    first commit

---
+++ C#/syntax/syntax/ZTesting.cs
@@ -0,0 +1,31 @@
+﻿
+
+// This file is only used for just testing of code and shit.
+
+
+namespace syntax
+{
+    internal class ZTesting
+    {
+        int Param { get; set; }
+
+        internal ZTesting(int param)
+        {
+            this.Param = param;
+        }
+    }
+
+    internal class XXX : ZTesting 
+    {
+        int Param2 { get; set; }
+
+        internal XXX(int param, int param2) : base(param)
+        {
+            this.Param2 = param2;
+        }
+
+    }
+
+
+}   
+
