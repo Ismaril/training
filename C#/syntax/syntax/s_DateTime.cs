13fa35e371b58d9f9ca9384cc6dba7c86386ac3f
tomlazen <oismarilo@gmail.com>
2023-03-23 22:00:28 +0100

    first commit

---
+++ C#/syntax/syntax/s_DateTime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syntax
+{
+    internal static class s_DateTime
+    {
+        internal static void Main__()
+        {
+            Utilities utility = new Utilities();
+            utility.Title("FILES");
+
+            DateTime birthYear = new DateTime(year: 1997, month: 1, day: 20);
+            Console.WriteLine(birthYear.DayOfWeek);
+
+            DateTime newDate = birthYear.AddYears(26);
+            newDate = newDate.AddMonths(2);
+            newDate = newDate.AddDays(3);
+            Console.WriteLine(newDate.Date);
+
+            utility.Separator();
+
+            TimeSpan myTimeSpan = new TimeSpan(
+                days: 31, hours: 23, minutes: 59, seconds: 10, milliseconds: 70
+                );
+            Console.WriteLine(myTimeSpan.ToString());
+
+
+
+            Console.WriteLine(myTimeSpan.ToString());
+        }
+    }
+}
