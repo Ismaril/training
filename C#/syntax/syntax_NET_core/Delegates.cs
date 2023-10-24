e62d80ae0984e276623d7d1ca477ced591f297d1
tomlazen <oismarilo@gmail.com>
2023-10-24 22:00:58 +0200

    first commit

---
+++ C#/syntax/syntax_NET_core/Delegates.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syntax_NET_core
+{
+    internal class Delegates
+    {
+        // Delegates are used to pass methods as arguments to other methods.
+        public static void Main__()
+        {
+            Utilities utilities = new();
+            utilities.PrintLine();
+
+            Console.WriteLine(Sum_(10, 20));
+
+            utilities.PrintLine();
+
+            // Create an instance of the delegate with a function passed inside.
+            SumDelegate sumDelegate = new(Sum_);
+            Console.WriteLine(sumDelegate(10, 20));
+            MaxDelegate maxDelegate = new(Max_);
+            Console.WriteLine(maxDelegate(10, 20));
+
+            utilities.PrintLine();
+
+            // COMBINE DELEGATES
+            // Below you will see also use of invocation list. This allows you to invoke/execute
+            // all the functions when you have combined multiple delegates.
+
+            // Use just one function passed inside.
+            DoSomethingDelegate doSomethingDelegate = new(DoSomething);
+            Console.WriteLine(doSomethingDelegate());
+
+            // Unnecessary long syntax to combine two delegates.
+            doSomethingDelegate = (DoSomethingDelegate)DoSomethingDelegate.Combine(
+                    new DoSomethingDelegate(DoSomething),
+                    new DoSomethingDelegate(DoSomethingElse)
+                    );
+            foreach (DoSomethingDelegate item in doSomethingDelegate.GetInvocationList())
+                Console.WriteLine(item());
+
+            // Shorter syntax to combine two delegates.
+            doSomethingDelegate = new DoSomethingDelegate(DoSomething);
+            doSomethingDelegate += new DoSomethingDelegate(DoSomethingElse);
+            foreach (DoSomethingDelegate item in doSomethingDelegate.GetInvocationList())
+                Console.WriteLine(item());
+
+            // Even shorter syntax to combine two delegates.
+            doSomethingDelegate = DoSomething;
+            doSomethingDelegate += DoSomethingElse;
+            foreach (DoSomethingDelegate item in doSomethingDelegate.GetInvocationList())
+                Console.WriteLine(item());
+
+            utilities.PrintLine();
+
+        }
+
+        private static int Sum_(int x, int y)
+        {
+            return x + y;
+        }
+
+        private static int Max_(int x, int y)
+        {
+            return x > y ? x : y;
+        }
+
+        private static string DoSomething()
+        {
+            return "Doing somehthing";
+        }
+
+        private static string DoSomethingElse()
+        {
+            return "Doing somehthing else";
+        }
+
+        // Create a delegate.
+        private delegate int SumDelegate(int x, int y);
+        private delegate int MaxDelegate(int x, int y);
+        private delegate string DoSomethingDelegate();
+    }
+}
---
+++ C#/syntax/syntax_NET_core/NullableValueTypes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syntax_NET_core
+{
+    internal class NullableValueTypes
+    {
+        public static void Main__()
+        {
+            Utilities utilities = new();
+            utilities.PrintLine();
+
+            //int i = null; // This is not allowed, because int is a value type.
+
+            // Both mean the same thing.
+            int? i = null; // This is allowed, because int? is a nullable value type.
+            Nullable<int> j = null; // This is allowed, because Nullable<int> is a nullable value type.
+            Console.WriteLine($"i: {i}.");
+            Console.WriteLine($"j: {j}.");
+
+            // Operation over null value of the nullable type will always be null again.
+            Console.WriteLine($"Result: {i + 1}.");
+
+            // Check here, that it really does not have any value.
+            if (i.HasValue)
+                Console.WriteLine($"i: {i.Value}");
+            else
+                Console.WriteLine("i is null.");
+
+            // You can also use the GetValueOrDefault() method.
+            // Into the parameter of GetValueOrDefault() method you can pass a default value,
+            //  of your choice.
+            Console.WriteLine($"Default value: {i.GetValueOrDefault()}.");
+            Console.WriteLine($"Default value: {i.GetValueOrDefault(5)}.");
+            // Shorter syntax.
+            Console.WriteLine($"Default value: {i ?? 0}.");
+            Console.WriteLine($"Default value: {i ?? 5}.");
+
+
+        }
+
+
+    }
+}
