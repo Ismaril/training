c7b9780cf239a0627cc380690eff47966a13e8f6
tomlazen <oismarilo@gmail.com>
2023-11-20 19:17:53 +0100

    First commit

---
+++ C#/syntax/syntax_NET_core/PreprocessorDirectives.cs
@@ -0,0 +1,50 @@
+﻿// Uncomment if you do not have it defined in configuration manager.
+//# define TRIAL 
+
+# define RELEASE
+
+namespace syntax_NET_core
+{
+    internal static class PreprocessorDirectives
+    {
+        public static void Main__()
+        {
+            // This code block will be compiled only if the Trial is defined
+            //  at the top of this file.
+            // Also this Trial must be defined in every file where you want to use it.
+            // But it is better idea to define it in the options, you can do it
+            //  in the box where you see Debug or Release. Just add new configuration.
+#if TRIAL
+            Console.WriteLine("Trial version");
+#endif
+
+            // Conditional compilation
+#if ENTERPRISE
+            Console.WriteLine("Enterprise version");
+#elif PROFESSIONAL
+            Console.WriteLine("Professional version");
+#endif
+
+#if ENTERPRISE || PROFFESIONAL
+            Console.WriteLine("You got it all");
+#endif
+            // Warning directive
+#if DEBUG && RELEASE
+#warning "You cannot define both DEBUG and RELEASE"
+#endif
+            // Error directive
+#if TRIAL && RELEASE
+#line 100 // You can specify the line number which will be shown along error msg.
+#error "You cannot define both DEBUG and RELEASE" 
+#endif
+        }
+
+        // You can use conditional attribute to mark a
+        //  method that should be called only in debug mode for example.
+        [System.Diagnostics.Conditional("DEBUG")]
+        static void WriteShit(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ C#/syntax/syntax_NET_core/Records.cs
@@ -0,0 +1,63 @@
+﻿/*
+RECORDS
+
+A record in C# is a class or struct that provides special 
+    syntax and behavior for working with data models. 
+The record modifier instructs the compiler to synthesize members 
+    that are useful for types whose primary role is storing data. 
+These members include an overload of ToString() and members that 
+    support value equality.
+
+WHEN TO USE RECORDS
+Use a record in place of a class or struct in the following scenarios:
+You want to define a data model that depends on value equality.
+You want to define a type for which objects are immutable.
+
+VALUE EQUALITY
+For records, value equality means that two variables of a 
+    record type are equal if the types match and all property 
+    and field values match. 
+For other reference types such as classes, equality means reference equality. 
+That is, two variables of a class type are equal if they refer to the same object. 
+Methods and operators that determine equality of two record instances use 
+    value equality.
+
+Not all data models work well with value equality. 
+For example, Entity Framework Core depends on reference equality 
+    to ensure that it uses only one instance of an entity type for 
+    what is conceptually one entity. For this reason, 
+    record types aren't appropriate for use as entity types in 
+    Entity Framework Core.
+*/
+
+namespace syntax_NET_core
+{
+    internal static class Records
+    {
+        // In my own words, it seems that it makes sense to use records
+        //  when you want to compare two objects that have same
+        //  immutable parametes and you do not care about the
+        //  reference equality.
+        public static void Main__()
+        {
+            var car1 = new Car("Toyota", "Corolla", 2020);
+            var car2 = new Car("Toyota", "Corolla", 2020);
+            var car3 = new Car("Corolla", "Toyota", 2020);
+
+            Console.WriteLine(car1 == car2); // Outputs: True
+            Console.WriteLine(car1 == car3); // Outputs: False
+
+            Console.WriteLine(car1.Make); // Outputs: Toyota
+
+            // In C#, the with keyword is used with records to create a new
+            //  instance that is a copy of an existing instance with some properties modified.
+            // This is known as non-destructive mutation.
+            // The with keyword is part of the record's syntax that supports immutability.
+            var car4 = car1 with { Year = 2021 };
+            Console.WriteLine(car4.Year); // Outputs: 2021
+        }
+
+    }
+    public record Car(string Make, string Model, int Year);
+
+}
