054f445ed103f8bd26ab7d62e768948b0a55141b
tomlazen <oismarilo@gmail.com>
2024-06-24 21:49:08 +0200

    renamed file

--- Others/design_patterns/Builder/HouseBuilders.cs
+++ Others/design_patterns/Builder/HouseBuilders.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Builder
 {
-    // 3. IMPLEMENT CONCRETE BUILDER
+    // 3. IMPLEMENT CONCRETE BUILDER (Concrete meaning "beton")
+    // Example 1 of builder.
     // -----------------------------------------------------------------
 
     internal class ConcreteHouseBuilder: IHouseBuilder
@@ -26,6 +27,33 @@ namespace Builder
         public void BuildWindows()
         {
             _house.Windows = "Double-glazed";
+        }
+
+        public House GetHouse()
+        {
+            return _house;
+        }
+    }
+
+    // Example 2 of builder.
+    // Builder for wooden houses
+    internal class WoodenHouseBuilder : IHouseBuilder
+    {
+        private House _house = new House();
+
+        public void BuildWalls()
+        {
+            _house.Walls = "Wooden walls";
+        }
+
+        public void BuildRoof()
+        {
+            _house.Roof = "Wooden";
+        }
+
+        public void BuildWindows()
+        {
+            _house.Windows = "Single-glazed";
         }
 
         public House GetHouse()
