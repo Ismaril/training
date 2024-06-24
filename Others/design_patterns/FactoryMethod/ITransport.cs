5cd24e0388e4ac0f6299f9fd0283f96477802681
tomlazen <oismarilo@gmail.com>
2024-06-24 21:50:04 +0200

    first commit

---
+++ Others/design_patterns/FactoryMethod/ITransport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    // 1. DEFINE THE PRODUCT INTERFACE:
+    public interface ITransport
+    {
+        void Deliver();
+    }
+}
---
+++ Others/design_patterns/FactoryMethod/Transporters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    // 2. IMPLEMENT CONCRETE PRODUCTS:
+
+    public class Truck : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Deliver by land in a truck.");
+        }
+    }
+
+    public class Ship : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Deliver by sea in a ship.");
+        }
+    }
+}
