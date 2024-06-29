7a664ab6b91c5052dafa406c84b348add84291e8
tomlazen <oismarilo@gmail.com>
2024-06-29 08:57:16 +0200

    first commit

---
+++ Others/DesignPatterns/ConsoleOutputSeparator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public static class ConsoleOutputSeparator
+    {
+        const string text = "----------------------------------------------------";
+
+        public static void Separator()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
