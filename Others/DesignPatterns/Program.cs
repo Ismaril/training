91f89cd144eed054a223601d7dd2816ff1be04c7
tomlazen <oismarilo@gmail.com>
2024-07-09 17:41:58 +0200

    merge conflict fix

--- Others/DesignPatterns/Program.cs
+++ Others/DesignPatterns/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             // CREATIONAL PATTERNS
+            // These patterns provide various object creation mechanisms,
+            // which increase flexibility and reuse of existing code.
             //ProgramFactoryMethod.Main__();
             //ProgramAbstractFactory.Main__();
             //ProgramBuilder.Main__();
@@ -12,18 +14,24 @@
             //ProgramSingleton.Main__();
 
             // STRUCTURAL PATTERNS
+            // These patterns explain how to assemble objects and classes
+            // into larger structures while keeping these structures
+            // flexible and efficient.
             //ProgramAdapter.Main__();
             //ProgramDecorator.Main__();
             //ProgramFacade.Main__();
             //ProgramProxy.Main__();
             //ProgramComposite.Main__();
             //ProgramBridge.Main__();
-            ProgramFlyweight.Main__();
+            //ProgramFlyweight.Main__();
 
             // BEHAVIORAL PATTERNS
-
+            // These patterns are concerned with algorithms and
+            // the assignment of responsibilities between objects.
+            ProgramChainOfResponsibility.Main__();
 
             Console.ReadLine();
+
         }
     }
 }
