c3351477b218bfdafffc05f7db412e519dd1a5e4
tomlazen <oismarilo@gmail.com>
2024-07-09 17:42:20 +0200

    first commit

---
+++ Others/DesignPatterns/ChainOfResponsibility.cs
@@ -0,0 +1,111 @@
+﻿// COMPLEXITY OF PATTERN: MEDIUM
+
+
+namespace DesignPatterns
+{
+    // The Handler interface declares a method for building the chain of
+    // handlers. It also declares a method for executing a request.
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        object Handle(object request);
+    }
+
+    // The default chaining behavior can be implemented inside a base handler
+    // class.
+    abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+        public IHandler SetNext(IHandler handler)
+        {
+            _nextHandler = handler;
+
+            // Returning a handler from here will let us link handlers in a
+            // convenient way like this:
+            // monkey.SetNext(squirrel).SetNext(dog);
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            if (_nextHandler != null)
+                return _nextHandler.Handle(request);
+            else
+                return null;
+        }
+    }
+
+    class MonkeyHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if ((request as string) == "Banana")
+                return $"Monkey: I'll eat the {request.ToString()}.\n";
+            else
+                return base.Handle(request);
+        }
+    }
+
+    class SquirrelHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "Nut")
+                return $"Squirrel: I'll eat the {request.ToString()}.\n";
+            else
+                return base.Handle(request);
+        }
+    }
+
+    class DogHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "MeatBall")
+                return $"Dog: I'll eat the {request.ToString()}.\n";
+            else
+                return base.Handle(request);
+        }
+    }
+
+    class Client__
+    {
+        // The client code is usually suited to work with a single handler. In
+        // most cases, it is not even aware that the handler is part of a chain.
+        public static void ClientCode(AbstractHandler handler)
+        {
+            var list_ = new List<string> { "Nut", "Banana", "Cup of coffee" };
+            foreach (var food in list_)
+            {
+                Console.WriteLine($"Client: Who wants a {food}?");
+
+                var result = handler.Handle(food);
+
+                if (result != null)
+                    Console.Write($"   {result}");
+                else
+                    Console.WriteLine($"   {food} was left untouched.");
+            }
+        }
+    }
+    public class ProgramChainOfResponsibility
+    {
+        public static void Main__()
+        {
+            // The other part of the client code constructs the actual chain.
+            MonkeyHandler monkey = new();
+            SquirrelHandler squirrel = new();
+            DogHandler dog = new();
+
+            monkey.SetNext(squirrel).SetNext(dog);
+
+            // The client should be able to send a request to any handler, not
+            // just the first one in the chain.
+            Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+            Client__.ClientCode(monkey);
+            Console.WriteLine();
+            Console.WriteLine("Subchain: Squirrel > Dog\n");
+            Client__.ClientCode(squirrel);
+        }
+    }
+}
---
+++ Others/DesignPatterns/Command.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// COMPLEXITY OF PATTERN: EASY
+
+namespace DesignPatterns
+{
+    // ---------------------------------------------------------------------------------
+    // 1. Define the Command Interface
+    public interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+
+    // ---------------------------------------------------------------------------------
+    // 2. CREATE CONCRETE COMMANDS
+    public class LightOnCommand(Light light) : ICommand
+    {
+        public void Execute() => light.On();
+        public void Undo() => light.Off();
+    }
+
+    public class LightOffCommand(Light light) : ICommand
+    {
+        public void Execute() => light.Off();
+        public void Undo() => light.On();
+    }
+
+    // ---------------------------------------------------------------------------------
+    // 3. DEFINE THE RECEIVER
+    public class Light
+    {
+        public void On() => Console.WriteLine("The light is on");
+        public void Off() => Console.WriteLine("The light is off");
+    }
+
+    // ---------------------------------------------------------------------------------
+    // 4. DEFINE THE INVOKER
+    public class RemoteControl
+    {
+        private ICommand _command;
+        public void SetCommand(ICommand command) => _command = command;
+        public void PressButton() => _command.Execute();
+        public void PressUndo() => _command.Undo();
+    }
+
+    public class ProgramCommand
+    {
+        public static void Main__()
+        {
+            // Create the receiver
+            Light livingRoomLight = new();
+
+            // Create commands
+            ICommand lightOn = new LightOnCommand(livingRoomLight);
+            ICommand lightOff = new LightOffCommand(livingRoomLight);
+
+            // Create invoker
+            RemoteControl remote = new();
+
+            // Turn the light on
+            remote.SetCommand(lightOn);
+            remote.PressButton(); // Output: The light is on
+
+            // Turn the light off
+            remote.SetCommand(lightOff);
+            remote.PressButton(); // Output: The light is off
+
+            // Undo the last action
+            remote.PressUndo(); // Output: The light is on
+        }
+    }
+}
