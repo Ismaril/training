4422c421b12da6cade003ec8ca58178956155776
TomLazn <laznicka.tomas@siemens.com>
2024-07-12 14:35:51 +0200

    added some more explanation and better examples

--- Others/DesignPatterns/Program.cs
+++ Others/DesignPatterns/Program.cs
@@ -28,7 +28,9 @@
             // BEHAVIORAL PATTERNS
             // These patterns are concerned with algorithms and
             // the assignment of responsibilities between objects.
-            ProgramChainOfResponsibility.Main__();
+            //ProgramChainOfResponsibility.Main__();
+            //ProgramCommand.Main__();
+            ProgramIterator.Main__();
 
             Console.ReadLine();
 
