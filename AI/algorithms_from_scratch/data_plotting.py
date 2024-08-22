579b35494d4ca19107b56be1315c37d17df78ee9
tomlazen <oismarilo@gmail.com>
2024-08-22 18:20:01 +0200

    Refactoring

--- AI/algorithms_from_scratch/data_plotting.py
+++ AI/algorithms_from_scratch/data_plotting.py
@@ -1,85 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.base import BaseEstimator
-from my_extensions import MyModelExtensions
 
-# class Plots:
-#     def __init__(self):
-#         ...
-#
-#     def plot_decision_boundary(self,
-#                                model,
-#                                X,
-#                                y,
-#                                bound_smoot=100,
-#                                fig_size=(16, 9),
-#                                grid=True,
-#                                verbose=False,
-#                                cmap="brg"):
-#         """
-#         :param model: model that's going to predict y from X
-#         :param X: features
-#         :param y: labels
-#         :param bound_smoot: smoothness of decision boundary, higher number higher smoothness
-#         :param fig_size: size of figure, enter two value tuple
-#         :param grid: if True place grid on plot
-#         :param verbose: print examples how internal data look like
-#         :param cmap: colormap from matplotlib
-#         """
-#         model.fit(X, y)
-#
-#         # Create a 2D grid of points (Xx, Xy) that we're going to predict on
-#         x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
-#         x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
-#         Xx, Xy = np.meshgrid(np.linspace(x1_min, x1_max, bound_smoot),
-#                              np.linspace(x2_min, x2_max, bound_smoot))
-#
-#         # Create X values (we're going to predict on all of these)
-#         # np.c_ makes columns from data inside its square brackets
-#         # ravel flattens multidimensional array
-#         X_meshgrid = np.c_[Xx.ravel(), Xy.ravel()]
-#
-#         # Make predictions using the trained model on "grid of X values" and predict
-#         #   y labels. X_meshgrid (created by np.linspace) has equally separated values
-#         #   that create a grid across the whole plot that you gonna see, therefore
-#         #   there will be number of prediction equal to the number of points of grid
-#         #   of X_meshgrid. Therefore if we gonna predict y_labels on all grid values of X_meshgrid
-#         #   we gonna see that underlying colors (based on predicted labels) will be
-#         #   extending everywhere to the point where the prediction is another label. This
-#         #   means that the decision boundaries on the plot are real and predictions of
-#         #   hypothetical points on either on side or others should really label the feature (X)
-#         #   as different class
-#         y_pred = model.predict(X_meshgrid)
-#         y_pred = y_pred.reshape(Xx.shape)
-#
-#         plt.figure(figsize=fig_size)
-#         plt.grid(grid)
-#
-#         # Plot decision boundary
-#         plt.contourf(Xx,  # x coordinates of features
-#                      Xy,  # y coordinates of features
-#                      y_pred,  # labels of features
-#                      cmap=cmap,
-#                      alpha=0.5)  # set how dark should underling color be
-#
-#         plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1],
-#                     s=180,
-#                     facecolors='#FFFF00',
-#                     zorder=-1)  # Makes the circles behind the data points
-#
-#         # plot points
-#         plt.scatter(X[:, 0],
-#                     X[:, 1],
-#                     c=y,
-#                     s=40,
-#                     cmap=cmap,
-#                     edgecolors="k")
-#         plt.show()
-#
-#         if verbose:
-#             print("xx, yy, x_in, y_pred")
-#             print(Xx.shape, Xy.shape, X_meshgrid.shape, y_pred.shape, sep="\n" * 2)
-#             print(Xx[0], Xy[0], X_meshgrid[0], y_pred[0], sep="\n" * 2)
 
 class Plots:
     def __init__(self):
@@ -93,7 +14,8 @@ class Plots:
                                fig_size=(16, 9),
                                grid=True,
                                verbose=False,
-                               cmap="brg"):
+                               cmap="brg",
+                               support_vectors=False):
         """
         :param model: model that's going to predict y from X
         :param X: features
@@ -103,19 +25,22 @@ class Plots:
         :param grid: if True place grid on plot
         :param verbose: print examples how internal data look like
         :param cmap: colormap from matplotlib
+        :param support_vectors: if True plot support vectors, applicable only for Support Vector Machines
         """
         model.fit(X, y)
 
         # Create a 2D grid of points (Xx, Xy) that we're going to predict on
         x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
         x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+        # Xx and Xy are 2D matrices.
+        # Each point from Xx and Xy together form a distinct coordinate.
         Xx, Xy = np.meshgrid(np.linspace(x1_min, x1_max, bound_smoot),
                              np.linspace(x2_min, x2_max, bound_smoot))
 
-        # Create X values (we're going to predict on all of these)
+        # Create X data (we're going to predict on all of these)
         X_meshgrid = np.c_[Xx.ravel(), Xy.ravel()]
 
-        # Make predictions using the trained model on "grid of X values"
+        # Make predictions using the trained model on "matrix of X values"
         y_pred = model.predict(X_meshgrid)
         y_pred = y_pred.reshape(Xx.shape)
 
@@ -125,51 +50,29 @@ class Plots:
         # Plot decision boundary
         plt.contourf(Xx, Xy, y_pred, cmap=cmap, alpha=0.5)
 
-        # Plot support vectors
-        plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1],
-                    s=180, facecolors='#FFFF00', zorder=-1)  # Makes the circles behind the data points
+        if support_vectors:
+            # Plot support vectors
+            plt.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1],
+                        s=180, facecolors='#FFFF00', zorder=-1)  # Makes the circles behind the data points
 
-        # -----------------------------------------------
+            w = model.coef_[0]
+            b = model.intercept_[0]
 
-        w = model.coef_[0]
-        b = model.intercept_[0]
+            # At the decision boundary, w0*x0 + w1*x1 + b = 0
+            # => x1 = -w0/w1 * x0 - b/w1
+            x0 = np.linspace(x1_min, x1_max, 200)
+            decision_boundary = -w[0] / w[1] * x0 - b / w[1]
 
-        # At the decision boundary, w0*x0 + w1*x1 + b = 0
-        # => x1 = -w0/w1 * x0 - b/w1
-        x0 = np.linspace(x1_min, x1_max, 200)
-        decision_boundary = -w[0] / w[1] * x0 - b / w[1]
+            margin = 1 / w[1]
+            gutter_up = decision_boundary + margin
+            gutter_down = decision_boundary - margin
+            svs = model.support_vectors_
 
-        margin = 1 / w[1]
-        gutter_up = decision_boundary + margin
-        gutter_down = decision_boundary - margin
-        svs = model.support_vectors_
-
-        plt.plot(x0, decision_boundary, "k-", linewidth=2, zorder=-2)
-        plt.plot(x0, gutter_up, "k--", linewidth=2, zorder=-2)
-        plt.plot(x0, gutter_down, "k--", linewidth=2, zorder=-2)
-        plt.scatter(svs[:, 0], svs[:, 1], s=180, facecolors='#FF0000',
-                    zorder=-1)
-        # -----------------------------------------------
-
-        # # Plot margin lines
-        # w = model.coef_  # Get the weights from the model
-        # slope = -w[0] / w[1]
-        # intercept = -model.intercept_[0] / w[1]
-        #
-        # # Calculate the margin distance (1 / ||w||)
-        # margin = 1 / np.sqrt(np.sum(w ** 2))
-        #
-        # # Plot the decision boundary line (center)
-        # x1_vals = np.linspace(x1_min, x1_max, bound_smoot)
-        # decision_boundary = slope * x1_vals + intercept
-        # plt.plot(x1_vals, decision_boundary, 'k-')
-        #
-        # # Plot the parallel lines (margins)
-        # margin_line_above = decision_boundary + margin
-        # margin_line_below = decision_boundary - margin
-        #
-        # plt.plot(x1_vals, margin_line_above, 'k--')
-        # plt.plot(x1_vals, margin_line_below, 'k--')
+            plt.plot(x0, decision_boundary, "k-", linewidth=2, zorder=-2)
+            plt.plot(x0, gutter_up, "k--", linewidth=2, zorder=-2)
+            plt.plot(x0, gutter_down, "k--", linewidth=2, zorder=-2)
+            plt.scatter(svs[:, 0], svs[:, 1], s=180, facecolors='#FF0000',
+                        zorder=-1)
 
         # Plot points
         plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=cmap, edgecolors="k")
--- AI/algorithms_from_scratch/my_extensions.py
+++ AI/algorithms_from_scratch/my_extensions.py
@@ -1,10 +1,33 @@
-import matplotlib.pyplot as plt
+from importlib.metadata import requires
 
-class MyModelExtensions:
+import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
+
+
+class MyModelBase(ABC):
+    @abstractmethod
+    def show_parameters(self):
+        ...
+
+    @abstractmethod
+    def show_loss_curve(self):
+        ...
+
+    @abstractmethod
+    def fit(self, X, y):
+        ...
+
+    @abstractmethod
+    def predict(self, X):
+        ...
+
+
+class MyRegressionBase(MyModelBase):
     """
     This class is a collection of methods that can be used to extend the functionality of the models.
     Used in Regression models.
     """
+
     def __init__(self):
         """
         Initialize the class.
@@ -18,7 +41,6 @@ class MyModelExtensions:
         # Here you can append the loss of each iteration of the fit method.
         # Later you can access this to visualize the loss curve during time.
         self.losses = []
-
 
     def show_parameters(self):
         """
@@ -38,4 +60,52 @@ class MyModelExtensions:
         plt.show()
 
     def fit(self, X, y):
+        ...
+
+    def predict(self, X):
+        ...
+
+
+class MySVMBase(MyModelBase):
+    """
+    This class is a collection of methods that can be used to extend the functionality of the models.
+    Used in Support Vector Machines.
+    """
+
+    def __init__(self):
+        """
+        Initialize the class.
+        """
+
+        # parameters learned by the model.
+        self.coef_ = None
+        self.intercept_ = None
+        self.support_vectors_ = None
+
+        # Here you can append the loss of each iteration of the fit method.
+        # Later you can access this to visualize the loss curve during time.
+        self.Js = []
+
+    def show_parameters(self):
+        """
+        Show the parameters/thetas learned by the model.
+        :return: List of thetas
+        """
+
+        return f"coef_:\n{self.coef_}\n\nintercept_:\n{self.intercept_}\n\nsupport_vectors_:\n{self.support_vectors_}"
+
+    def show_loss_curve(self):
+        """
+        Show the loss curve. Call this method after the fit method to see the full history of the loss.
+        :return: None
+        """
+        plt.plot(self.Js)
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss')
+        plt.show()
+
+    def fit(self, X, y):
+        ...
+
+    def predict(self, X):
         ...
