94951d6650d7c75f1c21c2b732babffb88bdef4c
TomLazn <laznicka.tomas@siemens.com>
2025-02-05 14:50:45 +0100

    Add option to hibernate and terminate after certain time

--- Utilities/MouseMover/Program.cs
+++ Utilities/MouseMover/Program.cs
@@ -1,19 +1,40 @@
-using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 
 class Program
 {
+    // OPERATE HERE ---------------------------------------------------------------------------------------
+    static readonly bool ExecuteForever = true;
+    static readonly bool HibernatePC = false; // Will not hibernate if ExecuteForever is true.
+    static readonly int TimerTillExitProgramMinutes = 10; // Will not be used if ExecuteForever is true.
+    static readonly int HowLongBeforeCursorMovesSeconds = 10;
+    // ----------------------------------------------------------------------------------------------------
+
     [DllImport("user32.dll")]
     static extern void SetCursorPos(int x, int y);
 
     static void Main()
     {
-        Random random = new Random();
-        while (true)
+        const int MILISECONDS_IN_SECOND = 1000;
+        const int SECONDS_IN_MINUTE = 60;
+
+        const int SCREEN_WIDTH = 1920;
+        const int SCREEN_HEIGHT = 1080;
+
+        Random random = new();
+        int timerTillExitSeconds = SECONDS_IN_MINUTE * TimerTillExitProgramMinutes;
+        int counterSeconds = 0;
+
+        while (counterSeconds < timerTillExitSeconds || ExecuteForever)
         {
-            SetCursorPos(random.Next(0, 1920), random.Next(0, 1080));
-            Thread.Sleep(10_000);
+            SetCursorPos(random.Next(0, SCREEN_WIDTH), random.Next(0, SCREEN_HEIGHT));
+            if (!ExecuteForever)
+                counterSeconds += HowLongBeforeCursorMovesSeconds;
+            Thread.Sleep(HowLongBeforeCursorMovesSeconds * MILISECONDS_IN_SECOND);
         }
+
+        // Hibernate the PC if the flag is set. No timer, hibernate immediately.
+        if (HibernatePC)
+            Process.Start("shutdown", "/h");
     }
 }
